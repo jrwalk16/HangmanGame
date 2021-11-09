1ae88786660fcff2a00cea6c034a0db689aad172
jrwalk16 <jeremywalker1216@Gmail.com>
2021-11-09 08:25:45 -0500

    Add project files.

---
+++ HangmanGame/HangmanGame.py
@@ -0,0 +1,66 @@
+
+import random
+
+hang = ["""
+H A N G M A N - Olympics
+   +---+
+   |   |
+       |
+       |
+       |
+       |
+=========""", """
+H A N G M A N - Olympics
+
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========""", """
+H A N G M A N - Olympics
+
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========""", """
+H A N G M A N - Olympics
+
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========""", """
+H A N G M A N - Olympics
+
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========""", """
+H A N G M A N - Olympics
+
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========""", """
+H A N G M A N - Olympics
+
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+========="""]
